ad1432e8f805db2249be2dbf5c2a10b8ce3e1346
ailmari <Arttu.Niemela@student.oulu.fi>
2016-09-20 18:13:24 +0300

    Add files via upload

--- test0/src/main.cpp
+++ test0/src/main.cpp
@@ -10,9 +10,9 @@ using namespace std;
 int main() {
 
 	// Open video stream
-	VideoCapture videostream(0);
+	VideoCapture videoStream(0);
 	
-	if (!videostream.isOpened()) {
+	if (!videoStream.isOpened()) {
 		cout << "Sorry, camera not found" << endl;
 		return 1;
 		}
@@ -20,24 +20,44 @@ int main() {
 	// Grab and show picture
 	Mat rawBGR;
 	Timer timer = Timer();
+	int frameCounter = 0;
+	double videoStreams[100];
+	double imageShows[100];
+	double totalTimes[100];
 	
 	while (true) {
 		timer.reset();
+		frameCounter = ++frameCounter % 100;
 		
-		videostream >> rawBGR;
+		videoStream >> rawBGR;
 		timer.update();
-		cout << timer.getDelta() << "s - read videostream" << endl;
+		videoStreams[frameCounter] = timer.getDelta();
+		//cout << timer.getDelta() << "s - read videostream" << endl;
 		
 		imshow("test", rawBGR);
 		timer.update();
-		cout << timer.getDelta() << "s - show image" << endl;
+		imageShows[frameCounter] = timer.getDelta();
+		//cout << timer.getDelta() << "s - show image" << endl;
 		
-		cout << timer.getTotal() << "s - total time" << endl << endl;
+		totalTimes[frameCounter] = timer.getTotal();
+		//cout << timer.getTotal() << "s - total time" << endl << endl;
 
-		if (waitKey(10) == 27) {
+		if (waitKey(1) == 27) {
 			break;
 		}
 	}
 	
+	double sumVideoStreams = 0;
+	double sumImageShows = 0;
+	double sumTotalTimes = 0;
+	for (int i = 0; i < 100; i++) {
+		double sumVideoStreams =+ videoStreams[i];
+		double sumImageShows =+ imageShows[i];
+		double sumTotalTimes =+ totalTimes[i];
+		
+		avgVideoStreams = sumVideoStreams / 100;
+		avgImageShows = sumImageShows / 100;
+		avgTotalTimes = sumTotalTimes / 100;
+	}
 	return 0;
 }
