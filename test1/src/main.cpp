c31f9831a5676d704ce02cb8705c86da9041d047
ailmari <Arttu.Niemela@student.oulu.fi>
2016-09-20 19:02:48 +0300

    Add files via upload

--- test1/src/main.cpp
+++ test1/src/main.cpp
@@ -8,40 +8,70 @@ using namespace cv;
 using namespace std;
 
 int main() {
-
-	// Open video stream
-	VideoCapture videostream(0);
 	
-	if (!videostream.isOpened()) {
+	// Open video stream
+	VideoCapture videoStream(0);
+	if (!videoStream.isOpened()) {
 		cout << "Sorry, camera not found" << endl;
 		return 1;
 		}
-	
-	double videoWidth = videostream.get(CV_CAP_PROP_FRAME_WIDTH);
-	double videoHeight = videostream.get(CV_CAP_PROP_FRAME_HEIGHT);
-
-	// Grab and show picture
+		
+	// Main loop
 	Mat rawBGR;
+	
 	Timer timer = Timer();
+	
+	int frameCounter = 0;
+	
+	double videoStreams[100];
+	double imageShows[100];
+	double totalTimes[100];
 	
 	while (true) {
 		timer.reset();
 		
-		videostream >> rawBGR;
-		timer.update();
-		cout << timer.getDelta() << "s - read videostream" << endl;
+		frameCounter = (frameCounter + 1) % 100;
 		
+		// Grab raw image from videostream
+		videoStream >> rawBGR;
+		timer.update();
+		videoStreams[frameCounter] = timer.getDelta();
+		
+		// Show image on screen
 		imshow("test", rawBGR);
 		timer.update();
-		cout << timer.getDelta() << "s - show image" << endl;
+		imageShows[frameCounter] = timer.getDelta();
 		
-		cout << timer.getTotal() << "s - total time" << endl << endl;
+		totalTimes[frameCounter] = timer.getTotal();
 
 		if (waitKey(1) == 27) {
-			cout << videoHeight << " x " << videoWidth << endl;
 			break;
 		}
 	}
+	
+	// Count execution times
+	double sumVideoStreams = 0;
+	double sumImageShows = 0;
+	double sumTotalTimes = 0;
+	double avgVideoStreams;
+	double avgImageShows;
+	double avgTotalTimes;
+	
+	for (int i = 0; i < 100; i++) {
+		sumVideoStreams += videoStreams[i];
+		sumImageShows += imageShows[i];
+		sumTotalTimes += totalTimes[i];
+		
+		avgVideoStreams = sumVideoStreams / 100.0;
+		avgImageShows = sumImageShows / 100.0;
+		avgTotalTimes = sumTotalTimes / 100.0;
+	}
+	
+	// Print execution times
+	cout << avgVideoStreams << "s - read videostream" << endl;
+	cout << avgImageShows << "s - show image" << endl;
+	cout << avgTotalTimes << "s - total time" << endl;
+	cout << 1.0 / avgTotalTimes << " fps" << endl << endl;
 	
 	return 0;
 }
